974b361fd9b7b460b4c3bfde7d33eaff21b568b5
DELL <marwa.jabroun.sordo@gmail.com>
2024-06-02 13:08:45 +0100

    hello person

---
+++ src/main/java/com/learning/demo/HelloPerson.java
@@ -0,0 +1,15 @@
+package com.learning.demo;
+/**
+ * @Author : Maroua Jabroun
+ * @Description : Person Information
+ */
+public class HelloPerson {
+    public static void main(String[] args) {
+        System.out.println("Name: Lisa");
+        System.out.println("Last Name:Ha");
+        System.out.println("Age: 18");
+        System.out.println("Adresse: Tetouan-Lhemama");
+        System.out.println("Favorite Color: Black-Pink");
+    }
+
+}
